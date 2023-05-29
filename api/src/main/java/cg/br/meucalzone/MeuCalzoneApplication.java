4271c5cf8927b15cda58515dea3781e4972801db
marquescauee <cauelopesmarques123@gmail.com>
2023-05-29 19:35:25 -0300

    Start

---
+++ api/src/main/java/cg/br/meucalzone/MeuCalzoneApplication.java
@@ -0,0 +1,13 @@
+package cg.br.meucalzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuCalzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuCalzoneApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/cg/br/meucalzone/MeuCalzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package cg.br.meucalzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuCalzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
