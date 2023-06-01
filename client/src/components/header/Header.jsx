f9bcc12489b5067e876a2932ce7e996bccaf762c
marquescauee <cauelopesmarques123@gmail.com>
2023-06-01 15:36:43 -0300

    Fixing Link Title

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -8,12 +8,12 @@ const Header = () => {
         <div>
             <header className="d-flex flex-wrap align-items-center py-2 mb-3 line-header">
                 <nav className="w-75 m-auto d-flex align-items-center">
-                    <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-light text-decoration-none navTitle">
+                    <Link to={"/"} className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-light text-decoration-none navTitle">
                         <span className="fs-4 text-light">
                             <img src="assets/calzone_logo2.png" width="80" alt="" />
                         </span>
                         <span className="navMeuCalzone">Meu Calzone :)</span>
-                    </a>
+                    </Link>
                     <ul className="nav nav-pills">
                         <li className="nav-item">
                             <Link className="nav-link text-light" to={"/"}>Home</Link>
