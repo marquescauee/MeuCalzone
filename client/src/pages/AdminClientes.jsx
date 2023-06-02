6435be927bb32ea183a85d96922ab1421a753647
marquescauee <cauelopesmarques123@gmail.com>
2023-06-02 12:43:31 -0300

    Switch CSS File

--- client/src/pages/AdminClientes.jsx
+++ client/src/pages/AdminClientes.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import Header from "../components/header/headerAdmin/Header"
 import Footer from "../components/footer/Footer"
-import '../App.css'
+import '../General.css'
 
 const AdminClientes = () => {
     return (
