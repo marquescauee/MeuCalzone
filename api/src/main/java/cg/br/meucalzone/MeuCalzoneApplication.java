9995bff6eca13bcd66f75a20e4a8597f80d4c976
marquescauee <cauelopesmarques123@gmail.com>
2023-06-06 15:13:17 -0300

    CORS Config

--- api/src/main/java/cg/br/meucalzone/MeuCalzoneApplication.java
+++ api/src/main/java/cg/br/meucalzone/MeuCalzoneApplication.java
@@ -2,9 +2,23 @@ package cg.br.meucalzone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MeuCalzoneApplication {
+
+	@Bean
+	public WebMvcConfigurer configCORS( ) {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuCalzoneApplication.class, args);
